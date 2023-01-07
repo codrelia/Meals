c67d2d7873518df2f2bc33f99313a934b8bfb423
Dasha Shevchenko <68771978+codrelia@users.noreply.github.com>
2023-01-07 14:01:35 +0300

    Adding TabBar

--- Meals/Application/MealsApp.swift
+++ Meals/Application/MealsApp.swift
@@ -4,7 +4,10 @@ import SwiftUI
 struct MealsApp: App {
     var body: some Scene {
         WindowGroup {
-            MealsViews()
+            NavigationView {
+                MealsViews()
+                    .navigationTitle("Meals")
+            }
         }
     }
 }
